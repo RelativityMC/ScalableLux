c8ae85a88f1be156864b7b1120859f3c3cab638f
ishland <ishlandmc@yeah.net>
2024-12-31 15:56:56 +0800

    fix: script quirk

--- scripts/init.sh
+++ scripts/init.sh
@@ -27,7 +27,7 @@ function cleanupPatches() {
     gitver=$(tail -n 2 "$patch" | grep -ve "^$" | tail -n 1)
     diffs=$(git diff --staged "$patch" | grep -E "^(\+|\-)" | grep -Ev "(From [a-z0-9]{32,}|\-\-\- a|\+\+\+ b|.index|Date\: )")
 
-    testver=$(echo "$diffs" | tail -n 2 | grep -ve "^$" | tail -n 1 | grep "$gitver")
+    testver=$(echo "$diffs" | tail -n 2 | grep -ve "^$" | tail -n 1 | grep -- "$gitver")
     if [ "x$testver" != "x" ]; then
       diffs=$(echo "$diffs" | tail -n +3)
     fi
